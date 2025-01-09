a4fb1c3ab805428321b21d13643f118ebe987d1a
Macbook <akindele@omnnixbms.com>
2025-01-09 16:16:01 +0100

    added more pictures to the gallery section

---
+++ polos.js
@@ -0,0 +1,34 @@
+const polos = () => {
+
+    const data = [
+        {
+            picture: 'images/pollo/pollo-1.jpg',
+            price: '₦56',
+            name: 'Suit'
+        },
+        
+    ];
+
+      // Select the container where products will be added
+      const container = document.getElementById('polos-container');
+
+      // Generate HTML for each product and append to the container
+      const productsHTML = data.map(el => `
+        <div class="col-sm-3">
+            <div class="product-image-wrapper">
+                <div class="single-products">
+                    <div class="productinfo text-center">
+                        <img src="${el.picture}" alt="${el.name}" />
+                        <h2></h2>
+                        <a href="#call-2" class="btn btn-default add-to-cart">Get it now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+      `).join('');
+
+      container.innerHTML = productsHTML;
+
+}
+
+polos();
---
+++ suits.js
@@ -0,0 +1,48 @@
+const suits = () => {
+
+    const data = [
+        {
+            picture: 'images/suits/suit-1.jpg',
+            price: '₦56',
+            name: 'Suit'
+        },
+        {
+            picture: 'images/suits/suit-2.jpg',
+            price: '₦56',
+            name: 'Suit'
+        },
+        {
+            picture: 'images/suits/suit-3.jpg',
+            price: '₦56',
+            name: 'Suit'
+        },
+        {
+            picture: 'images/suits/suit-4.jpg',
+            price: '₦56',
+            name: 'Suit'
+        },
+    ];
+
+      // Select the container where products will be added
+      const container = document.getElementById('suits-container');
+
+      // Generate HTML for each product and append to the container
+      const productsHTML = data.map(el => `
+        <div class="col-sm-3">
+            <div class="product-image-wrapper">
+                <div class="single-products">
+                    <div class="productinfo text-center">
+                        <img src="${el.picture}" alt="${el.name}" />
+                        <h2></h2>
+                        <a href="#call-2" class="btn btn-default add-to-cart">Get it now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+      `).join('');
+
+      container.innerHTML = productsHTML;
+
+}
+
+suits();
